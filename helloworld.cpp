4f5aa57080624d890d9bdfffc28d3dd0662b6c44
emirayemr <106332223+emirayemr@users.noreply.github.com>
2022-06-22 17:20:09 +0300

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Emir Ay on 9.08.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
